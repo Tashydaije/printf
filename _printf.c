6f47b7842e8e24dc2f709f6aa521a1b3f64d834f
Tashydaije <mbuguanatasha01@gmail.com>
2023-08-21 17:02:15 -0700

    function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _printf -  function that produces output according to a format
+ * @format: A character string
+ *
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int printed_chars = 0;
+	const char *ptr = format;
+
+	handler_t handlers[] = {
+		 {'c', handle_char},
+		 {'s', handle_string},
+		 {'%', handle_percent}
+	};
+
+	va_start(args, format);
+
+	while (*ptr)
+	{
+		unsigned int found_handler = 0;
+
+		if (*ptr != '%')
+		{
+			write(1, ptr, 1);
+			printed_chars++;
+		} else
+		{
+			ptr++;
+
+			if (*ptr == '%')
+			{
+				write(1, ptr, 1);
+				printed_chars++;
+			} else if (*ptr)
+			{
+				long unsigned int i;
+				for (i = 0; i < sizeof(handlers) / sizeof(handlers[0]); i++)
+				{
+					if (*ptr == handlers[i].specifier)
+					{
+						printed_chars += handlers[i].handler(args);
+						found_handler = 1;
+						break;
+					}
+				}
+			}
+			if (!found_handler && *ptr != '%')
+			{
+				write(1, "%", 1);
+				printed_chars++;
+			}
+		}
+		ptr++;
+	}
+	va_end(args);
+	return (printed_chars);
+}
