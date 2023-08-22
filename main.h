1ffc9da59f20b5b635a7f02ffdf8456a2d4c0ec1
Tashydaije <mbuguanatasha01@gmail.com>
2023-08-21 17:02:56 -0700

    header file containing func prototypes and struct

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct handler - A new type defining a conversion handler struct
+ * @specifier: A char representing a conversion specifier
+ * @handler: A pointer to a conversion handler func for a specifier
+ */
+
+typedef struct handler_s
+{
+	unsigned char specifier;
+	unsigned int (*handler)(va_list arg);
+} handler_t;
+
+
+/* prototypes for used functions */
+int _printf(const char *format, ...);
+
+unsigned int handle_char(va_list arg);
+unsigned int handle_string(va_list arg);
+unsigned int handle_percent();
+
+#endif
